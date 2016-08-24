9a7944ae1783a339cc57fba8e119a39bc7eadcf2
jnburchett <jnburchett@yahoo.com>
2016-08-24 13:58:24 -0400

    added 'fixLines', a script to write a new parameter table with columns fixed

---
+++ joebvp/fixLines.py
@@ -0,0 +1,30 @@
+######################################################
+#
+# Script to take parameter file output from VPmeasure,
+# set parameter ties to 'fixed', and write new output.
+#
+######################################################
+
+from astropy.io import ascii
+
+def fixinput(fname,outfile='fixedVP.dat',fixpars=['n','b','v']):
+    inp=ascii.read(fname)
+    if isinstance(fixpars,list):
+        for col in fixpars:
+            if col not in ['n','b','v','N']:
+                raise ValueError('Unknown line parameter.  Please use any of \'n\', \'N\', \'b\', \'v\'.')
+            else:
+                if col=='N': col='n'
+                colname=col+'flag'
+                inp[colname]=1
+    else:
+        if fixpars not in ['n', 'b', 'v', 'N']:
+            raise ValueError('Unknown line parameter.  Please use any of \'n\', \'N\', \'b\', \'v\'.')
+        else:
+            if fixpars == 'N': fixpars = 'n'
+            colname = fixpars + 'flag'
+            inp[colname] = 1
+    ascii.write(inp,outfile,delimiter='|')
+
+
+
