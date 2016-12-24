9f081e3694efa12ef4672239107c09ba923725d2
jnburchett <jnburchett@yahoo.com>
2016-12-24 10:10:18 -0500

    new utils function to return nonzero fitting errors from lines tied together in fit

--- joebvp/utils.py
+++ joebvp/utils.py
@@ -85,9 +85,9 @@ def abslines_from_VPfile(parfile,ra=None,dec=None):
     ----------
     parfile : str
         Name of the parameter file in the joebvp format
-    ra : float
+    ra : float, optional
         Right Ascension of the QSO in decimal degrees
-    dec : float
+    dec : float, optional
         Declination of the QSO in decimal degress
 
     Returns
@@ -97,19 +97,108 @@ def abslines_from_VPfile(parfile,ra=None,dec=None):
     '''
     from linetools.spectralline import AbsLine
     import astropy.units as u
-    fitpars, fiterrors, parinfo = joebvpfit.readpars(parfile)
-    linetab = ascii.read(parfile)
-    abslinelist = []
+    linetab = ascii.read(parfile) # Read parameters from file
+    abslinelist = [] # Initiate list to populate
     for i,row in enumerate(linetab):
+        ### Check to see if errors for this line are defined
+        colerr,berr,velerr=get_errors(linetab,i)
+        ### Adjust velocity limits according to centroid errors and limits from file
+        vcentmin = row['vel']-velerr
+        vcentmax = row['vel']+velerr
+        v1 = vcentmin + row['vlim1']
+        v2 = vcentmax + row['vlim2']
         line=AbsLine(row['restwave']*u.AA, z=row['zsys'])
-        vlims=[row['vlim1'],row['vlim2']]*u.km/u.s
+        vlims=[v1,v2]*u.km/u.s
         line.limits.set(vlims)
+        ### Set other parameters
         line.attrib['logN'] = row['col']
-        line.attrib['sig_N'] = row['sigcol']
+        line.attrib['sig_N'] = colerr
         line.attrib['b'] = row['bval']
-        line.attrib['sig_b'] = row['sigbval']
+        line.attrib['sig_b'] = berr
+        ### Add it to the list and go on
         abslinelist.append(line)
     return abslinelist
+
+def get_errors(partable,idx2check):
+    '''
+    Get actual error values from VP fitting.
+    When lines are tied in fit, algorithm returns '0' for all lines but 1 in fit.
+    This routine will return errors from row in table that actually have errors.
+
+    Parameters
+    ----------
+    partable : astropy Table
+        Imported Table from parameter file in the joebvp format
+    idx2check : int
+        Index of row to
+    Returns
+    -------
+    colerr : float
+        Fitting error of column density
+    berr: float
+        Fitting error of Doppler b value
+    velerr: float
+        Fitting error of velocity centroid
+    '''
+
+    row = partable[idx2check]
+
+    ### Process column density, Doppler b value, then velocity centroid
+    if row['sigcol']==0:  # Check to see if errors for this line are defined
+        simulfit=np.where((partable['nflag']==row['nflag'])&(partable['zsys']==row['zsys']))[0] # Find lines tied to this one
+        if len(simulfit)>0: # Are there any?
+            simulfit_nz=simulfit[np.where(partable['nflag'][simulfit]!=0)[0]]  # Find the one(s) with nonzero errors
+            if len(simulfit_nz)>0: # Are there any?
+                colerr=partable['sigcol'][simulfit_nz[0]] # Adopt nonzero error of this row
+            else:
+                colerr=row['sigcol'] # No nonzero errors; accept value from table read in
+        else:
+            colerr=row['sigcol'] # No rows tied to this one; accept value from table read in
+    else:
+        colerr=row['sigcol'] # This row has nonzero errors; accept value from table read in
+        
+    if row['sigbval']==0:
+        simulfit=np.where((partable['bflag']==row['bflag'])&(partable['zsys']!=row['zsys']))[0]
+        if len(simulfit)>0:
+            simulfit_nz=simulfit[np.where(partable['bflag'][simulfit]!=0)[0]]
+            if len(simulfit_nz)>0:
+                berr=partable['sigbval'][simulfit_nz[0]]
+            else:
+                berr=row['sigbval']
+        else:
+            berr=row['sigbval']
+    else:
+        berr=row['sigbval']
+
+    if row['sigvel']==0:
+        simulfit=np.where((partable['vflag']==row['vflag'])&(partable['zsys']!=row['zsys']))[0]
+        if len(simulfit)>0:
+            simulfit_nz=simulfit[np.where(partable['vflag'][simulfit]!=0)[0]]
+            if len(simulfit_nz)>0:
+                velerr=partable['sigvel'][simulfit_nz[0]]
+            else:
+                velerr=row['sigvel']
+        else:
+            velerr=row['sigvel']
+    else:
+        velerr=row['sigvel']
+
+    return colerr, berr, velerr
+    
+#def inspect_fits(parfile):
+    '''
+    Produce pdf of fitting results  for quality check.
+
+    Parameters
+    ----------
+    parfile : str
+        Name of the parameter file in the joebvp format
+
+    Returns
+    -------
+
+    '''
+
 
 
 
